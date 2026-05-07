a4289d2826d7b5473968874f38a1837a610e5423
MrMisterBean <alutter03@gmail.com>
2026-05-07 17:51:48 -0400

    added a circle with WASD direction controls

--- main.c
+++ main.c
@@ -11,14 +11,39 @@ int main(void) {
   InitWindow(screenWidth, screenHeight, "game window");
   SetTargetFPS(60);
 
+  const int speed = 100;
+  Vector2 deltaCircle = {0, (float)screenHeight/50.0f}; 
+
   while(!WindowShouldClose()) // while the window shouldn't be closing (due to x, alt+f4, etc.)
   {
     // Update variables here:
+
+    bool w_pressed = IsKeyDown(0x57);
+    bool a_pressed = IsKeyDown(0x41);
+    bool s_pressed = IsKeyDown(0x53);
+    bool d_pressed = IsKeyDown(0x44);
+
+    if (w_pressed){
+      deltaCircle.y -= GetFrameTime()*speed;
+    }
+
+    if (a_pressed){
+      deltaCircle.x -= GetFrameTime()*speed;
+    }
+
+    if (s_pressed){
+      deltaCircle.y += GetFrameTime()*speed;
+    }
+
+    if (d_pressed){
+      deltaCircle.x += GetFrameTime()*speed;
+    }
 
     BeginDrawing();
 
       ClearBackground(RAYWHITE);
       DrawText("Hello world!", 200, 200, 20, BLACK);
+      DrawCircleV(deltaCircle, 5, RED);
 
     EndDrawing();
   }
