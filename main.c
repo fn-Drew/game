c355a8c69a5a6d2535886180936ed213275312b0
MrMisterBean <alutter03@gmail.com>
2026-05-07 22:45:21 -0400

    abstracted player logic with structs

--- main.c
+++ main.c
@@ -4,47 +4,73 @@
 #include <math.h>
 #include <raylib.h>
 
+#define KEY_COUNT 4
+
+const int screenWidth = 800;
+const int screenHeight = 800;
+
+int default_keys[4] = {'W','A','S','D'};
+typedef enum {W, A, S, D} PlayerKey;
+
+typedef struct {
+  int keys[KEY_COUNT];
+  bool keys_pressed[KEY_COUNT];
+  int radius;
+  int speed;
+  Vector2 position;
+} Player;
+
+void initializePlayer(Player *player) {
+  for(int key = 0; key < KEY_COUNT; key++){
+    player->keys[key] = default_keys[key];
+  }
+  player->position = (Vector2){(float)screenWidth/2.0f, (float)screenHeight/2.0f};
+  player->radius = 5;
+  player->speed= 250;
+}
+
+void updatePlayer(Player *player) {
+  for(int key = 0; key < KEY_COUNT; key++){
+    player->keys_pressed[key] = IsKeyDown(player->keys[key]);
+  }
+}
+
 int main(void) {
-  const int screenWidth = 800;
-  const int screenHeight = 800;
+  Player p1;
+  initializePlayer(&p1);
 
   InitWindow(screenWidth, screenHeight, "game window");
   SetTargetFPS(60);
 
   const int speed = 250;
-  Vector2 deltaCircle = {(float)screenWidth/2.0f, (float)screenHeight/2.0f}; 
   int deltaCircle_radius = 5;
 
   while(!WindowShouldClose()) // while the window shouldn't be closing (due to x, alt+f4, etc.)
   {
     // Update variables here:
+    updatePlayer(&p1);
 
-    bool w_pressed = IsKeyDown(0x57);
-    bool a_pressed = IsKeyDown(0x41);
-    bool s_pressed = IsKeyDown(0x53);
-    bool d_pressed = IsKeyDown(0x44);
-
-    if (w_pressed && deltaCircle.y > 0 + deltaCircle_radius){
-      deltaCircle.y -= GetFrameTime()*speed;
+    if (p1.keys_pressed[W] && p1.position.y > 0 + p1.radius){
+      p1.position.y -= GetFrameTime()*speed;
     }
 
-    if (a_pressed && deltaCircle.x > 0 + deltaCircle_radius){
-      deltaCircle.x -= GetFrameTime()*speed;
+    if (p1.keys_pressed[A] && p1.position.x > 0 + p1.radius){
+      p1.position.x -= GetFrameTime()*speed;
     }
 
-    if (s_pressed && deltaCircle.y < screenHeight - deltaCircle_radius){
-      deltaCircle.y += GetFrameTime()*speed;
+    if (p1.keys_pressed[S] && p1.position.y < screenHeight - p1.radius){
+      p1.position.y += GetFrameTime()*speed;
     }
 
-    if (d_pressed && deltaCircle.x < screenWidth - deltaCircle_radius){
-      deltaCircle.x += GetFrameTime()*speed;
+    if (p1.keys_pressed[D] && p1.position.x < screenWidth - p1.radius){
+      p1.position.x += GetFrameTime()*speed;
     }
 
     BeginDrawing();
 
       ClearBackground(RAYWHITE);
-      DrawText(TextFormat("Pos: %.1f, %.1f", deltaCircle.x, deltaCircle.y), 200, 200, 20, BLACK);
-      DrawCircleV(deltaCircle, 5, RED);
+      DrawText(TextFormat("Pos: %.1f, %.1f", p1.position.x + 20, p1.position.y + 20), 200, 200, 20, BLACK);
+      DrawCircleV(p1.position, 5, RED);
 
     EndDrawing();
   }
